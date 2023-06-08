4be35d70ce824c6ad935e93775c05acfda4c12af
vcpersike <vcpersike@gmail.com>
2023-06-08 04:19:38 -0300

    Ajustes nas buscas dos usuários do git

--- src/controllers/buscaUsuarioGitController.ts
+++ src/controllers/buscaUsuarioGitController.ts
@@ -3,12 +3,23 @@ import axios from 'axios';
 import firebase from '../services/firebase';
 
 const db = firebase.firestore();
+const usuariosGitCollection = db.collection('usuariosgit');
 
 async function buscarUsuario(request: FastifyRequest, reply: FastifyReply) {
     try {
         const { username } = request.params as any;
         const response = await axios.get(`https://api.github.com/users/${username}`);
-        await db.collection('usuarios').doc(username).set(response.data);
+
+        // Verificar se a coleção 'usuariosgit' existe
+        const collectionSnapshot = await usuariosGitCollection.get();
+        if (collectionSnapshot.empty) {
+            // A coleção não existe, então vamos criá-la
+            await usuariosGitCollection.doc(); // Criar um documento vazio para criar a coleção
+        }
+
+        // Agora podemos salvar os dados do usuário na coleção existente
+        await usuariosGitCollection.doc(username).set(response.data);
+
         reply.code(200).send(response.data);
     } catch (error) {
         console.error('Erro ao buscar o usuário:', error);
@@ -16,7 +27,19 @@ async function buscarUsuario(request: FastifyRequest, reply: FastifyReply) {
     }
 }
 
-export default {
-    buscarUsuario
+async function recuperarUsuarios(_: FastifyRequest, reply: FastifyReply) {
+    try {
+        const usuariosSnapshot = await usuariosGitCollection.get();
+        const usuarios = usuariosSnapshot.docs.map(doc => doc.data());
+
+        reply.code(200).send({ usuarios });
+    } catch (error) {
+        console.error('Erro ao recuperar os usuários:', error);
+        reply.code(500).send('Erro ao recuperar os usuários.');
+    }
 }
 
+export default {
+    buscarUsuario,
+    recuperarUsuarios
+}
--- src/routes/rotasSimples.ts
+++ src/routes/rotasSimples.ts
@@ -3,8 +3,9 @@ import fp from 'fastify-plugin';
 import buscaUsuarioGitController from '../controllers//buscaUsuarioGitController';
 
 const simplesRoutes: FastifyPluginCallback = (fastify, _, done) => {
-    fastify.get('/usuarios/:username', buscaUsuarioGitController.buscarUsuario)
-    done();
+    fastify.get('/usuariosgit/:username', buscaUsuarioGitController.buscarUsuario),
+        fastify.get('/usuariosgit', buscaUsuarioGitController.recuperarUsuarios),
+        done();
 }
 export default fp(simplesRoutes, { name: 'simplesRoutes' });
 
